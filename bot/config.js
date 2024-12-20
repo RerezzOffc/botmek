2e2da1711b8285b7ee94b033acd2ed6ef0e15aff
Rerezz Official <thegamergemu0001@gmail.com>
2024-12-20 20:47:53 +0700

    Create config.js

---
+++ bot/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  SERVER_URL: 'http://localhost:5000', // Alamat server Python
+  SYMBOL: 'EURUSD',                   // Pasangan mata uang
+  LOT: 0.1,                           // Ukuran lot
+};
